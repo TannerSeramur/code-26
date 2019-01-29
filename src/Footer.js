425ad89dc725361ef8a2b5dcf6abec089012aecd
Tanner Seramur <seramurt@gmail.com>
2019-01-29 09:38:46 -0800

    removed css outline border

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "../styles/footer.scss";
 
 class Footer extends React.Component {
   render() {
-    return <footer>&copy; 2018 Code Fellows</footer>;
+    return <footer>&copy; 2019 Code Fellows</footer>;
   }
 }
 
